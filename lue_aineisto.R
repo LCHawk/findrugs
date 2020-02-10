bf6c6afbe79bdaeec091d8f3bc39793e43568eb0
Kristian Vepsäläinen <kristian.vepsalainen@gmail.com>
2020-02-10 18:29:39 +0200

    Kaikki haut+metadata

--- lue_aineisto.R
+++ lue_aineisto.R
@@ -11,5 +11,8 @@ pakkaus_m<-read_delim(paste0(getwd(),"/Aineisto/pakkaus-m.txt"),";")
 pakkaus1<-read_delim(paste0(getwd(),"/Aineisto/pakkaus1.txt"),";")
 pakkaus_nolla<-read_delim(paste0(getwd(),"/Aineisto/pakkaus_nolla.txt"),";")
 names(pakkaus_nolla)<-paste0("nolla_",names(pakkaus_nolla))
-
-#Pakkaus1 &pakkaus0 samoja saadaan atc-tunnisteella kiinni atc-tauluun
+sailytysastia <- read_delim(paste0(getwd(),"/Aineisto/sailytysastia.txt"),";")
+#Pakkaus1 &pakkaus0 sekä pakkaus_m samoja saadaan atc-tunnisteella 
+#kiinni atc-tauluun
+#Pakkaus_m sisältää määräaikaiset lääkkeet, pakkaus1 nykytilan 
+#ja pakkaus0 lähtötilan käytä näistä pakkaus 1 ja pakkaus_m!
