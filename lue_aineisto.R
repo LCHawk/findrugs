f094657bc8996571d099961db202780078b192bd
Kristian Vepsäläinen <kristian.vepsalainen@gmail.com>
2020-02-10 19:51:03 +0200

    Kommentteja.

--- lue_aineisto.R
+++ lue_aineisto.R
@@ -8,6 +8,8 @@ ma_maaraamisehto<-read_delim(paste0(getwd(),
 names(ma_maaraamisehto)<-paste0("ma_",names(ma_maaraamisehto))
 maaraamisehto<-read_delim(paste0(getwd(),"/Aineisto/maaraamisehto.txt"),";")
 pakkaus_m<-read_delim(paste0(getwd(),"/Aineisto/pakkaus-m.txt"),";")
+names(pakkaus_m)<-paste0("maar_",names(pakkaus_m))
+
 pakkaus1<-read_delim(paste0(getwd(),"/Aineisto/pakkaus1.txt"),";")
 pakkaus_nolla<-read_delim(paste0(getwd(),"/Aineisto/pakkaus_nolla.txt"),";")
 names(pakkaus_nolla)<-paste0("nolla_",names(pakkaus_nolla))
@@ -16,3 +18,5 @@ sailytysastia <- read_delim(paste0(getwd(),"/Aineisto/sailytysastia.txt"),";")
 #kiinni atc-tauluun
 #Pakkaus_m sisältää määräaikaiset lääkkeet, pakkaus1 nykytilan 
 #ja pakkaus0 lähtötilan käytä näistä pakkaus 1 ja pakkaus_m!
+
+#Yhdistämisiä ei kuitenkaan välttämättä tarvita!
